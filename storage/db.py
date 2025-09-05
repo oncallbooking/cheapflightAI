0c0ea43e8a412f067c17f7eb3124e099a11ca876
oncallbooking <143792222+oncallbooking@users.noreply.github.com>
2025-09-05 11:53:35 +0530

    Create db.py

---
+++ storage/db.py
@@ -0,0 +1,7 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@db:5432/flights")
+engine = create_async_engine(DATABASE_URL, future=True, echo=False)
+AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
